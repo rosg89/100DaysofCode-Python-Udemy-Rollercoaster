a1459d5be831bd0946a91565afb338a675c08834
rosg89 <rsuarezgalan@hotmail.com>
2022-05-16 11:28:13 -0300

    Se sube el proyecto

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Welcome to the rollercoaster!")
+
+#Convierte el input de String a Int
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+  print("You can ride the rollercoaster! =)")
+else:
+  print("Sorry, you have to grow taller before you can ride =(")
+
+#Comparison Operators
+# > Greater than
+# < Less than
+# >= Greater than or equal to
+# <= Less than or equal to
+# == Equal to
+# != Not equal to
